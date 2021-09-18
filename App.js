6a97247e99f9473232dccccdb40d89da7ec106aa
Erica Carvalho <ericacrcarvalho@gmail.com>
2021-09-17 22:08:56 -0300

    aula

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Animated, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+ 
+  const [largura] = useState(new Animated.Value(0));
+  const [rotacao] = useState(new Animated.Value(0));
+
+  Animated.timing(largura, {toValue:80, duration:2000}).start();
+  Animated.timing(rotacao, {toValue:90, duration:2500}).start();
+
+  let larguraInterpolada = largura.interpolate({
+    inputRange: [0, 80],
+    outputRange: ['0%', '80%'],
+  });
+
+  let rotacaoInterpolada = rotacao.interpolate({
+    inputRange: [0, 90],
+    outputRange: ['0deg', '180deg'],
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Interpolação</Text>
+      <Animated.View
+        style={{width:larguraInterpolada, height:20, backgroundColor:'blue'}}
+      ></Animated.View>
+      <Animated.Text
+        style={{fontSize: 22, margin: 70, fontWeight:'bold', transform:[{rotate:rotacaoInterpolada}]}}
+      >Girando...</Animated.Text>
     </View>
   );
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 18,
+    fontWeight:'bold',
+  }
 });
